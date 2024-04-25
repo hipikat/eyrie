0492a2455b0dcb9ef92a2bc89c5d278b314374f9
Ada Wright <ada@hpk.io>
2024-04-25 13:55:51 +0800

    Get version from versioningit in eyrie.__about__

--- src/eyrie/__about__.py
+++ src/eyrie/__about__.py
@@ -1,4 +1,6 @@
 # SPDX-FileCopyrightText: 2024-present Ada Wright <ada@hpk.io>
-#
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.4"
+
+import versioningit
+
+__version__ = versioningit.get_version()
